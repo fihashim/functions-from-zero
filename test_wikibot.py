323692f1292e08d294f42bcb9f0120dc778a9166
Fairuz Hashim <fihashim@gmail.com>
2024-08-29 13:51:29 +0000

    original requirements and wikibot altered

--- test_wikibot.py
+++ test_wikibot.py
@@ -1,14 +1,4 @@
-from mylib.bot import scrape
-from wikibot import cli
-from click.testing import CliRunner
-
+from wikibot import scrape
 
 def test_scrape():
     assert "Microsoft" in scrape("Microsoft")
-
-
-def test_wikibot():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--name", "Microsoft"])
-    assert result.exit_code == 0
-    assert "Microsoft" in result.output
--- wikibot.py
+++ wikibot.py
@@ -1,13 +1,5 @@
-import click
-from mylib.bot import scrape
+import wikipedia
 
-
-@click.command()
-@click.option("--name", help="Web page we want to scrape")
-def cli(name):
-    result = scrape(name)
-    click.echo(click.style(f"{result}:", fg="blue"))
-
-
-if __name__ == "__main__":
-    cli()
+def scrape(name="Microsoft", length=1):
+    result = wikipedia.summary(name, sentences=length)
+    return result
